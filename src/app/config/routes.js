e8bfe2887f2abe814f1be327fe30df1beb484b74
kushBansal <bansal22kushi@gmail.com>
2020-01-09 00:01:54 +0530

    init commit

---
+++ src/app/config/routes.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Main from '../components/layouts/Main';
+import Blank from '../components/layouts/Blank';
+
+import MainView from '../views/Main';
+import MinorView from '../views/Minor';
+
+import { Route, Switch } from 'react-router-dom';
+
+export default (
+    <Switch>
+        <Route path="/" component={Main} />
+        {/* <IndexRedirect to="/main" /> */}
+        <Route path="main" component={MainView}> </Route>
+        <Route path="minor" component={MinorView}> </Route>
+    </Switch>
+);
